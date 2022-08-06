d407cfd3d548f842f0efaa86bbe72938b9033c47
Ilan Araujo <ilanrochaaraujo@gmail.com>
2022-08-06 01:44:37 -0300

    Criação dos modelos

--- hermes/cotacao/models.py
+++ hermes/cotacao/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    codigo = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.nome
+
+class Ativo(models.Model):
+    id = models.IntegerField(primary_key=True)
+    codigo = models.CharField(max_length=6)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    # Acrescentar método que diz o tipo de ação que o ativo é
+
+    def __str__(self):
+        return self.codigo
